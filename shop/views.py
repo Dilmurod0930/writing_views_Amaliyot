135a8762773187379191c297b0540dab1ea4cdc2
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-07-03 10:18:14 +0500

    Detail  page  uchun  funkiya  yozildi.

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Category
+from django.http  import  HttpResponse
 # Create your views here.
 
 def index(request):
@@ -12,3 +13,17 @@ def index(request):
 def about(request):
     return render(request, 'shop/about.html')
     
+
+def  product_detail(request, product_id):
+    try:
+
+        product = Product.objects.get(id=product_id)
+        context = {
+            'product': product
+        }
+    
+        return render(request, 'shop/detail.html', context)
+        
+
+    except  Product.DoesNotExist:
+        return  HttpResponse("Pradukt  Paje  Topilmadi. ")
