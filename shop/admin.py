7a1c626d7d4d1c08445079b799920790f6ded748
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-07-03 18:52:35 +0500

    Admin  Pagjega  o'rgarish kiritildi. Nomlari  o'zgartirildi.

--- shop/admin.py
+++ shop/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from  .models  import Product , Category
 # Register your models here.
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Category)
 
 
-admin.site.register(Product)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'discount', 'category')
-    list_filter = ('category',)
-    search_fields = ('name', 'description')
-    list_editable = ('price', 'discount')
+    list_display = ['name', 'price', 'discount', 'category']
+    list_filter = ['category',]
+    search_fields = ['name']
