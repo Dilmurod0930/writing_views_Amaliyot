a3da5c84dd272ea598f18d0a01d9a19fcfa293fd
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-07-04 21:22:54 +0500

    Katigoriyaga  oid  bo'lgan  malumotlari  ko'rishga  path  to'g'irlandi.

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from  .views  import index,  product_detail
 
 urlpatterns = [
     path('', index, name='index'),  # Shop ilovasining asosiy sahifasi
+    path('category/<int:catigorya_id>', index, name='category'),    # Kategoriyaga oid mahsulotlarni ko'rsatish 
     path('detail/<int:product_id>', product_detail,  name='product-detail')
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,26 +3,31 @@ from .models import Product, Category
 from django.http  import  HttpResponse
 # Create your views here.
 
-def index(request):
-    praducts = Product.objects.all()
-    context = {
-        'praducts': praducts
-    }
+def index(request,  catigorya_id=None):
+    category = Category.objects.all()
+    if catigorya_id:
+        praducts = Product.objects.filter(category = catigorya_id)
+    else:
+        praducts = Product.objects.all()
+        context = {
+            'praducts': praducts,
+            'categories': category
+        }
     return render(request, 'shop/index.html', context)
 
-def about(request):
-    return render(request, 'shop/about.html')
-    
+
+
+
 
 def  product_detail(request, product_id):
     try:
-
+        
         product = Product.objects.get(id=product_id)
         context = {
             'product': product
         }
     
-        return render(request, 'shop/detail.html', context)
+        return render(request, 'shop/product_detail.html', context)
         
 
     except  Product.DoesNotExist:
